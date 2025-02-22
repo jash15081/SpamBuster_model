a527a4d4871ae4b7f99b2d3523bf5c1d1897d802
Jaimin_Patel <jashdekavadiya@gmail.com>
2025-02-22 15:21:00 +0530

    model

---
+++ app.py
@@ -0,0 +1,32 @@
+# app.py
+
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+# Load the trained model
+model_path = 'spam_model.pkl'
+with open(model_path, 'rb') as file:
+    vectorizer,model = pickle.load(file)
+print(type(model)) 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Extract data from form
+    message = request.form.get('text')
+    message = vectorizer.transform([message])
+    
+    # Make prediction
+    prediction = model.predict(message)
+    output = 'Spam' if prediction[0] == 1 else 'not sapm :)'
+
+    return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
